201e0630f196f8ca5bf33d2acc97157eb2c8ed73
SunghyunKim9744 <dqq9744@naver.com>
2020-12-30 14:36:35 +0900

    12/30 ES6 Destructuring

--- es6/es6.js
+++ es6/es6.js
@@ -88,7 +88,7 @@ exam = {
     eng,
     math,
     total:function(){
-        // this - > exam 안에 있는 kor과 eng - > 40,41번째 줄
+        // this - > exam 안에 있는 kor과 eng - > 87,88번째 줄
         return this.kor+this.eng;
     }
 }
@@ -99,7 +99,7 @@ exam = {
     eng,
     math,
     total(){
-        // this - > exam 안에 있는 kor과 eng - > 40,41번째 줄
+        // this - > exam 안에 있는 kor과 eng - > 98,99번째 줄
         return this.kor+this.eng;
     }
 }
@@ -132,3 +132,163 @@ print();
 var exam3 = new Exam();
 console.log(exam3);
 console.log(`total is ${exam3.total()}`);
+
+
+// ========== Destructuring #1-1 =============
+{
+    let exam = {
+        kor:30,
+        eng:40,
+        math:50
+    };
+    
+    // let kor = exam.kor;
+    // let eng = exam.eng;
+    // console.log(`kor : ${kor}, eng : ${eng}`);
+
+    //  143~145 Destructuring을 아래와 같이 편하게 해줌
+    // exam안의 kor,eng를 지역변수로 만들어서 대입. 순서상관X
+    let {eng,kor} = exam;
+    console.log(`kor : ${kor}, eng : ${eng}`);
+
+// ========== Destructuring #1-2 =============
+// exam의 값이 변했을 때 대입 하는 방법 ()사용
+    exam.kor = 100;
+    exam.eng = 500;
+    ({eng,kor} = exam);
+    console.log(`kor : ${kor}, eng : ${eng}`);
+
+    exam.kor = 50;
+    ({kor}=exam);
+    console.log(`kor : ${kor}, eng : ${eng}`);
+}
+
+// ========== Destructuring #2 =============
+// 속성의 확장과 기본값
+
+{
+    let exam = {
+        kor:30,
+        eng1:40,
+        math:50
+    };
+
+    {
+         // exam에 없는 속성도 사용이 가능.
+    let {eng1,kor,total} = exam;
+    console.log(`kor : ${kor}, eng : ${eng1}, total : ${total}`);
+
+    // total에 값 대입
+    ({total=0}=exam);
+    console.log(`kor : ${kor}, eng : ${eng1}, total : ${total}`);
+    }
+   
+
+    // 속성이름 별칭주기
+    {
+        let {eng1:english,kor,total=0} = exam;
+        // eng1을 english로 사용해야함.
+        // console.log(`kor : ${kor}, eng : ${eng1}, total : ${total}`);
+        console.log(`kor : ${kor}, eng : ${english}, total : ${total}`);
+
+    }
+}
+
+// ========== Destructuring #3 =============
+// 중첩된 객체의 속성
+{
+    let exam = {
+        kor:30,
+        eng:40,
+        math:50,
+        student :{
+            name:"newlec",
+            phone:"010-1234-5678"
+        }
+    };
+
+    // exam안의 student
+    let {student} = exam;
+    console.log(student);
+    
+    {
+    // exam안의 student안의 name값 꺼내기.
+    let {student:{name}} = exam;
+    console.log(name); 
+    }
+    
+
+    let {kor,student:{name,phone}} = exam;
+    console.log(`kor : ${kor}, student name : ${name}, student phone : ${phone}`);
+}
+
+// ========== Destructuring #4 =============
+// 매개변수
+{
+    function printExam({kor,eng,math /*destructuring*/}) {
+        console.log(`kor : ${kor}, eng : ${eng}, math : ${math}`);
+    }
+    let {kor=10,eng=20,math=30} = {}; // destructuring
+    printExam({kor,eng,math}); // create Object
+}
+
+// ========== Destructuring #5 =============
+// Array destructuring
+{
+    let kors = [10,20,30];
+    let [k1,k2,k3] = kors;
+
+    console.log(`k1 : ${k1}, k2 : ${k2}, k3 : ${k3},`);
+
+    // 다른 값 대입
+    [k1] = [100,200,300]; // 100 대입
+    [,k2] = [100,200,300]; // 200 대입
+    [,,k3] = [100,200,300]; // 300 대입
+    console.log(`k1 : ${k1}, k2 : ${k2}, k3 : ${k3},`);
+
+    //꼼수 swapping
+    let x = 2;
+    let y = 3;
+    let z = 5;
+    console.log(`x:${x},y:${y},z:${z}`);
+    
+        //교체(Swap) 순서 재배열
+    //    let t = x;
+    //    x = y;
+    //    y = t;
+    [z, x, y] = [y, x, z];
+    console.log(`z:${z},y:${y},x:${x}`);
+
+    //인자의 수가 일치하지 않은 경우
+    //let [a,b,c,d=0] = kors;
+
+    //중첩 배열 뽀개기
+    {
+        let kors = [10,20,30,[40,50]];
+        let [
+            kor1,
+            kor2,
+            kor3,
+            [
+                kor4,
+                kor5
+            ]
+        ] = kors;
+
+        console.log(`${kor1},${kor2},${kor3},${kor4},${kor5}`);
+
+    }
+
+    // 객체와 배열 섞기
+    let notice = {
+        id:1,
+        title:"공지사항",
+        files:[
+            "img1.png",
+            "img2.png"
+        ]
+    };
+
+    let {id,title,files:[img1],files:[,img2]} = notice;
+    console.log(`id : ${id}, title : ${title}, files[0] : ${img1}, files[1] : ${img2}`);
+}
